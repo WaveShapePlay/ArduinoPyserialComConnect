b6ec99ce8a036ee9af769f78719a3ebfdf7f4a76
WaveShapePlay <WaveShapePlay@gmail.com>
2017-04-11 17:14:33 -0400

    Finding Arduino Serial to USB Com Port

---
+++ findArduino.py
@@ -0,0 +1,42 @@
+import serial.tools.list_ports
+
+def get_ports():
+
+    ports = serial.tools.list_ports.comports()
+    
+    return ports
+
+def findArduino(portsFound):
+    
+    commPort = 'None'
+    numConnection = len(portsFound)
+    
+    for i in range(0,numConnection):
+        port = foundPorts[i]
+        strPort = str(port)
+        
+        if 'Arduino' in strPort: 
+            splitPort = strPort.split(' ')
+            commPort = (splitPort[0])
+
+    return commPort
+            
+                    
+foundPorts = get_ports()        
+connectPort = findArduino(foundPorts)
+
+if connectPort != 'None':
+    ser = serial.Serial(connectPort,baudrate = 9600, timeout=1)
+    print('Connected to ' + connectPort)
+
+else:
+    print('Connection Issue!')
+
+print('DONE')
+
+
+
+
+
+    
+
